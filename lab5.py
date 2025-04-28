1fc56110c793d6158092ae0323a7fca0969d4dce
tieowg <mnuray2222@gmail.com>
2025-04-28 12:36:01 +0400

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -1,4 +1,5 @@
 import random
-arr = [random.randint(-10, 20) for _ in range(10)]
-greater_than_10 = [x for x in arr if x > 10]
-print(sum(greater_than_10) / len(greater_than_10) if greater_than_10 else "No elements greater than 10")
+n = int(input())
+arr = [random.randint(4, 47) for _ in range(n)]
+b = [x for x in arr if x % 3 == 1]
+print(eval('*'.join(map(str, b))) if b else "Yoxdur")
